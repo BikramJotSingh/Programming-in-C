441dc98dec1033ce269b26dc54f6178b4b79a411
BikramJotSingh <bikramjots9979@gmail.com>
2025-09-01 11:52:54 +0530

    Add files via upload

---
+++ Experiment 1.3.c
@@ -0,0 +1,20 @@
+//4. Write a C program to add two numbers, take number from user. 
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	int a,b,sum;
+	printf("Name - Bikram Jot Singh\nSAP ID - 590027095\nCourse-BCA\nBatch-5");
+	printf("\n.............................................................\n");
+	printf("Enter the first number......");
+	scanf("%d",&a);
+	printf("Enter the second number......");
+	scanf("%d",&b);
+	sum=a+b;
+	printf("The sum of two entered numbers is.......%d",sum);
+	getch();
+	return 0;
+	
+
+}
+
---
+++ Experiment 2.1.c
@@ -0,0 +1,24 @@
+/*Experiment 2: Operators 
+1. WAP a C program to calculate the area and perimeter of a rectangle based on
+its length and width.
+*/
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+	int length,width,area,perimeter;
+	printf("Name - Bikram Jot Singh\nSAP ID - 590027095\nCourse-BCA\nBatch-5");
+	printf("\n.................................................\n");
+	printf("Enter the length of a rectangle.................");
+	scanf("%d",&length);
+	printf("Enter the width of a rectangle........................");
+	scanf("%d",&width);
+	area=(length*width);
+	perimeter=(2*(length+width));
+	printf("The area of rectangle is...........%d\n",area);
+	printf("The perimeter of a rectangle is..................%d",perimeter);
+	return 0;
+	
+
+}
+
