0ee087de0a60f650fa1d84a3ef1f4b282bbbf267
BikramJotSingh <bikramjots9979@gmail.com>
2025-09-01 11:18:02 +0530

    Add files via upload

---
+++ Experiment 1.2.c
@@ -0,0 +1,12 @@
+//2. Write a C Program to print the address in multiple lines (new line). 
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	printf("Name - Bikram Jot Singh\nSAP ID - 590027095\nCourse-BCA\nBatch-5");
+	printf("\n...................................................\n");
+	printf("Kosi Farm\nbazpur\n(U.S Nagar)\nUttarakhand");
+	getch();
+	return 0;
+
+}
