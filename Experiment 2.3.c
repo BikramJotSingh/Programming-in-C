fcbc9eb53fe5d94fd95ef1adeac8418572e3e879
BikramJotSingh <bikramjots9979@gmail.com>
2025-09-01 12:33:08 +0530

    Add files via upload

---
+++ Experiment 2.3.c
@@ -0,0 +1,27 @@
+/*1. WAP a C program to calculate the Compound Interest by taking principle,rate and time from user.
+formula
+CI=principle*(pow((1+rate/100),time))-principle
+*/
+#include<stdio.h>;
+#include<math.h>;
+int main()
+{
+	double principle,rate,time;
+	int CI;
+	printf("Name - Bikram Jot Singh\nSAP ID - 590027095\nCourse-BCA\nBatch-5");
+	printf("\n..............................................\n");
+	printf("Enter the principle amount..............");
+	scanf("%lf",&principle);
+	printf("Enter the rate..................");
+	scanf("%lf",&rate);
+	printf("Enter the time..............");
+	scanf("%lf",&time);
+	CI=principle*(pow((1+rate/100),time))-principle; 
+	printf("The compound interset is...........................%d",CI);
+	return 0;
+
+
+}
+
+
+
