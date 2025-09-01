5e3eee08d075e8014a4b2d219e5fd885464646a6
BikramJotSingh <bikramjots9979@gmail.com>
2025-09-01 11:08:58 +0530

    Add files via upload

---
+++ Experiment 1.1.c
@@ -0,0 +1,14 @@
+/*Experiment 1: Installation, Environment Setup and starting with C language 
+1. Write a C program to print “ Hello World” */	
+
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	printf("Name - Bikram Jot Singh\nSAP ID - 590027095\nCourse-BCA\nBatch-5");
+	printf(".................\n...................\n");
+	printf("Hello World");
+	getch();
+	return 0;
+}
+
