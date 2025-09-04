66f452796987aa7aa377c7459162793ee3bddf83
BikramJotSingh <bikramjots9979@gmail.com>
2025-09-04 11:16:47 +0530

    Add files via upload

---
+++ Experiment 3.1.c
@@ -0,0 +1,20 @@
+/*Experiment:3.1 
+1.WAP of c to check whether a number is even or odd.
+*/
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	int n;
+	printf("Name - Bikram Jot Singh\nSAP ID - 590027095\nCourse-BCA\nBatch-5");
+	printf("\n......................................\n");
+	printf("Enter the number............");
+	scanf("%d",&n);
+	if((n%2)==0)
+	printf("Even %d",n);
+    else
+	{
+		printf("odd %d",n);
+	}
+	return 0;
+}
